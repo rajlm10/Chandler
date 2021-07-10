457280f1eaf1a1588300e86b2918a2dc56f5fdbd
Raj Sangani <rajbsangani@gmail.com>
2021-07-10 20:15:38 +0530

    final

---
+++ main.py
@@ -0,0 +1,284 @@
+#Importing libraries
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from nltk.stem import WordNetLemmatizer
+import nltk
+from nltk.corpus import stopwords
+import re
+import tensorflow_datasets as tfds
+from tensorflow.keras import layers
+from tensorflow.keras import backend as K
+import tensorflow_datasets as tfds
+import tensorflow as tf
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+import string
+import pickle
+from flask import Flask, render_template, url_for, request, redirect,jsonify
+
+app = Flask(__name__)
+
+
+
+nltk.download('wordnet')
+nltk.download('stopwords')
+
+
+#Utility functions to help preprocess text and map conntractions
+def decontract(text):
+    text = re.sub(r"won\'t", "will not", text)
+    text = re.sub(r"can\'t", "can not", text)
+    text = re.sub(r"n\'t", " not", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'s$", " is", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'t", " not", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'m", " am", text)
+    return text
+
+stopwords_english = set(stopwords.words('english'))-set(['No','no','not','Not'])
+
+def preprocess(text,stopwords=stopwords_english):
+    lemmatizer = WordNetLemmatizer()
+
+
+    # remove stock market tickers like $GE
+    text = re.sub(r'\$\w*', '', text)
+    # remove old style retweet text "RT"
+    text = re.sub(r'^RT[\s]+', '', text)
+    # remove hyperlinks
+    text = re.sub(r'https?:\/\/.*[\r\n]*', '', text)
+    #Decontract texts
+    text=decontract(text)
+    # tokenize texts
+
+
+    texts_clean = []
+    for word in text.split():
+        if (word not in stopwords_english and  # remove stopwords
+                word not in set(string.punctuation)-set(['!','?','.','@',':'])):  # remove punctuation
+            #Lemmatize word 
+            lem_word = lemmatizer.lemmatize(word,"v")  # Lemmatizing word
+            texts_clean.append(lem_word)
+
+    return " ".join(texts_clean)
+
+# Implemented based on the Attention is all you need paper.
+def scaled_dot_product_attention(queries, keys, values, mask):
+    # Calculating  QK.T
+    product = tf.matmul(queries, keys, transpose_b=True)
+    # Get the scale factor
+    keys_dim = tf.cast(tf.shape(keys)[-1], tf.float32)
+    # Scale the dot product for improved training speed and stability 
+    scaled_product = product / tf.math.sqrt(keys_dim)
+    # Apply the mask
+    if mask is not None:
+        scaled_product += (mask * -1e9)
+    # dot product of QK.T with Values 
+    attention = tf.matmul(tf.nn.softmax(scaled_product, axis=-1), values)
+    
+    return attention
+
+class MultiHeadAttention(layers.Layer):
+    
+    def __init__(self, n_heads):
+        super(MultiHeadAttention, self).__init__()
+        self.n_heads = n_heads
+        
+    def build(self, input_shape):
+        self.d_model = input_shape[-1]
+        assert self.d_model % self.n_heads == 0
+        # Calculate the dimension of every head or projection
+        self.d_head = self.d_model // self.n_heads
+        # Set the weight matrices for Q, K and V
+        self.query_weights = layers.Dense(units=self.d_model)
+        self.key_weights = layers.Dense(units=self.d_model)
+        self.value_weights = layers.Dense(units=self.d_model)
+        # Set the weight matrix for the output of the multi-head attention W0
+        self.final_weights = layers.Dense(units=self.d_model)
+        
+    def split_proj(self, inputs, batch_size): # inputs: (batch_size, seq_length, d_model)
+        # Set the dimension of the projections
+        shape = (batch_size,
+                 -1,
+                 self.n_heads,
+                 self.d_head)
+        # Split the input vectors
+        splited_inputs = tf.reshape(inputs, shape=shape) # (batch_size, seq_length, nb_proj, d_proj)
+        return tf.transpose(splited_inputs, perm=[0, 2, 1, 3]) # (batch_size, n_heads, seq_length, d_proj)
+    
+    def call(self, queries, keys, values, mask):
+        # Get the batch size
+        batch_size = tf.shape(queries)[0]
+        # Set the Query, Key and Value matrices
+        queries = self.query_weights(queries)
+        keys = self.key_weights(keys)
+        values = self.value_weights(values)
+        # Split Q, K y V between the heads or projections
+        queries = self.split_proj(queries, batch_size)
+        keys = self.split_proj(keys, batch_size)
+        values = self.split_proj(values, batch_size)
+        # Apply the scaled dot product
+        attention = scaled_dot_product_attention(queries, keys, values, mask)
+        # Get the attention scores
+        attention = tf.transpose(attention, perm=[0, 2, 1, 3])
+        # Concat the h heads or projections
+        concat_attention = tf.reshape(attention,
+                                      shape=(batch_size, -1, self.d_model))
+        # Apply W0 to get the output of the multi-head attention
+        outputs = self.final_weights(concat_attention)
+        
+        return outputs
+
+class PositionalEncoding(layers.Layer):
+
+    def __init__(self):
+        super(PositionalEncoding, self).__init__()
+    
+    def get_angles(self, pos, i, d_model): # pos: (seq_length, 1) i: (1, d_model)
+        angles = 1 / np.power(10000., (2*(i//2)) / np.float32(d_model))
+        return pos * angles # (seq_length, d_model)
+
+    def call(self, inputs):
+        # input shape batch_size, seq_length, d_model
+        seq_length = inputs.shape.as_list()[-2]
+        d_model = inputs.shape.as_list()[-1]
+        # Calculate the angles given the input
+        angles = self.get_angles(np.arange(seq_length)[:, np.newaxis],
+                                 np.arange(d_model)[np.newaxis, :],
+                                 d_model)
+        # Calculate the positional encodings
+        angles[:, 0::2] = np.sin(angles[:, 0::2])
+        angles[:, 1::2] = np.cos(angles[:, 1::2])
+        # Expand the encodings with a new dimension
+        pos_encoding = angles[np.newaxis, ...]
+        
+        return inputs + tf.cast(pos_encoding, tf.float32)
+
+class EncoderLayer(layers.Layer):
+    
+    def __init__(self, FFN_units, n_heads, dropout_rate):
+        super(EncoderLayer, self).__init__()
+        # Hidden units of the feed forward component
+        self.FFN_units = FFN_units
+        # Set the number of projectios or heads
+        self.n_heads = n_heads
+        # Dropout rate
+        self.dropout_rate = dropout_rate
+    
+    def build(self, input_shape):
+        self.d_model = input_shape[-1]
+        # Build the multihead layer
+        self.multi_head_attention = MultiHeadAttention(self.n_heads)
+        self.dropout_1 = layers.Dropout(rate=self.dropout_rate)
+        # Layer Normalization
+        self.norm_1 = layers.LayerNormalization(epsilon=1e-6)
+        # Fully connected feed forward layer
+        self.ffn1_relu = layers.Dense(units=self.FFN_units, activation="relu")
+        self.ffn2 = layers.Dense(units=self.d_model)
+        self.dropout_2 = layers.Dropout(rate=self.dropout_rate)
+        # Layer normalization
+        self.norm_2 = layers.LayerNormalization(epsilon=1e-6)
+        
+    def call(self, inputs, mask, training):
+        # Forward pass of the multi-head attention
+        attention = self.multi_head_attention(inputs,
+                                              inputs,
+                                              inputs,
+                                              mask)
+        attention = self.dropout_1(attention, training=training)
+        # Call to the residual connection and layer normalization
+        attention = self.norm_1(attention + inputs)
+        # Call to the FC layer
+        outputs = self.ffn1_relu(attention)
+        outputs = self.ffn2(outputs)
+        outputs = self.dropout_2(outputs, training=training)
+        # Call to residual connection and the layer normalization
+        outputs = self.norm_2(outputs + attention)
+        
+        return outputs
+
+class Encoder(layers.Layer):
+    
+    def __init__(self,
+                 n_layers,
+                 FFN_units,
+                 n_heads,
+                 dropout_rate,
+                 vocab_size,
+                 d_model,
+                 name="encoder"):
+        super(Encoder, self).__init__(name=name)
+        self.n_layers = n_layers
+        self.d_model = d_model
+        # The embedding layer
+        self.embedding = layers.Embedding(vocab_size, d_model)
+        # Positional encoding layer
+        self.pos_encoding = PositionalEncoding()
+        self.dropout = layers.Dropout(rate=dropout_rate)
+        # Stack of n layers of multi-head attention and FC
+        self.enc_layers = [EncoderLayer(FFN_units,
+                                        n_heads,
+                                        dropout_rate) 
+                           for _ in range(n_layers)]
+        self.final_layer = layers.Dense(units=2, name="final_layer")
+    
+    def call(self, inputs, mask, training):
+        # Get the embedding vectors
+        outputs = self.embedding(inputs)
+        # Scale the embeddings by sqrt of d_model
+        outputs *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
+        # Positional encodding
+        outputs = self.pos_encoding(outputs)
+        outputs = self.dropout(outputs, training)
+        # Call the stacked layers
+        for i in range(self.n_layers):
+            outputs = self.enc_layers[i](outputs, mask, training)
+        #print(outputs.shape)
+
+        logits=tf.math.reduce_mean(outputs,1)
+
+
+        logits=self.final_layer(logits)
+
+        return logits 
+
+def create_padding_mask(seq): #seq: (batch_size, seq_length)
+# Create the mask for padding
+  mask = tf.cast(tf.math.equal(seq, 0), tf.float32)
+  return mask[:, tf.newaxis, tf.newaxis, :]
+
+def predict(encoder,tokenized_sentences):
+  logits=encoder(tokenized_sentences,create_padding_mask(tokenized_sentences),False)
+  predictions=np.argmax(tf.keras.layers.Softmax()(logits),axis=1)
+  return predictions
+
+tokenizer=pickle.load(open('models/tokenizer.sav', 'rb'))
+loaded_model = pickle.load(open('models/finalized_model (1).sav', 'rb'))
+
+print("Tokenizer and Model Loaded")
+
+@app.route('/')
+def welcome():
+    return render_template('homePage.html')
+
+@app.route('/details', methods = ['POST', 'GET'])
+def details():
+    inputText = request.form['para']
+    sentence=tokenizer.encode(preprocess(inputText))
+    sentence=tf.keras.preprocessing.sequence.pad_sequences([sentence],value=0,padding='post',maxlen=50)
+
+    answer=predict(loaded_model,sentence)
+    if(answer)==1:
+        return render_template('details.html')
+    else:
+        return render_template('not.html')
+
+
+if __name__ == "__main__":
+    app.run(threaded=True,port=5000)
+
